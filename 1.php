d652b343b770fa1f75cfae654a543a45253d17a0
sled <max-30@mail.ru>
2019-03-25 19:16:11 +0400

    получил инфу от ресурса в хсоне

--- 1.php
+++ 1.php
@@ -1,27 +1,19 @@
 <?php
-
+require_once 'lib/function.php';
 //https://www.avito.ru/astrahanskaya_oblast/avtomobili/vaz_lada/2114_samara?s_trg=4&f=188_901b
+$site = 'https://www.avito.ru';
+$region = 'astrahanskaya_oblast';
+$category = 'avtomobili';
+$name_search = 'vaz_lada/2114_samara?s_trg=4&f=188_901b';
 
+$vaz = $site.'/'.$region.'/'.$category.'/'.$name_search;
+//var_dump(pars_find($vaz));
 
-
-
-
-$test=file_get_contents('https://www.avito.ru/astrahanskaya_oblast/avtomobili/vaz_lada/2114_samara?s_trg=4&f=188_901b');
-/*while(!feof($test))
-{
-  print_r($test);
-}*/
-//echo $test;
-//$match = '/\/favorites\/add\/([0-9]{10})/'; id обьявления
-//$match = '/content=\"([0-9]{1,20})"/'; /// цена
-$match = '/itemprop="url"
- href=\"(.{3,})\"/';
-preg_match_all($match, $test, $text);
-print_r($text);
-foreach($text[1] as $keys)
-{
-  // code...
-  echo "avito.ru".$keys."\n";
-}
+/*$avto_link = $site.'/'.$region.'/'.$category.'/'.vaz_2114_samara_2009_1713497498'
+$model_avto= 'vaz_2114_samara_';*/
+$a = 'https://m.avito.ru/liman/avtomobili/vaz_2114_samara_2009_1274909156';
+$jsovn = find_data_avto(file_get_contents($a));
+$w = derban_json($jsovn);
+var_dump($w->item);
 
 ?>
