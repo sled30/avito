0bbd986e77ec39beae5d292c8383045b8c53a28e
sled <max-30@mail.ru>
2019-04-08 22:42:46 +0400

    разобрал структуру хсона

--- 1.php
+++ 1.php
@@ -1,5 +1,11 @@
 <?php
 require_once 'lib/function.php';
+
+/*
+
+                            pars_find->
+
+*/
 //https://www.avito.ru/astrahanskaya_oblast/avtomobili/vaz_lada/2114_samara?s_trg=4&f=188_901b
 $site = 'https://www.avito.ru';
 $region = 'astrahanskaya_oblast';
@@ -7,13 +13,16 @@ $category = 'avtomobili';
 $name_search = 'vaz_lada/2114_samara?s_trg=4&f=188_901b';
 
 $vaz = $site.'/'.$region.'/'.$category.'/'.$name_search;
-//var_dump(pars_find($vaz));
+//echo $vaz;
+avito_parser_auto($vaz);
 
 /*$avto_link = $site.'/'.$region.'/'.$category.'/'.vaz_2114_samara_2009_1713497498'
 $model_avto= 'vaz_2114_samara_';*/
-$a = 'https://m.avito.ru/liman/avtomobili/vaz_2114_samara_2009_1274909156';
-$jsovn = find_data_avto(file_get_contents($a));
-$w = derban_json($jsovn);
-var_dump($w->item);
+//$a = 'https://m.avito.ru/liman/avtomobili/vaz_2114_samara_2009_1274909156';
+//$jsovn = find_data_avto(file_get_contents($a));
+//$w = derban_json($jsovn);
+
+
+
 
 ?>
--- lib/function.php
+++ lib/function.php
@@ -1,23 +1,45 @@
 <?php
+// avito_parser_auto->pars_find->pars_car->->--->
+//   ->
+
+function avito_parser_auto($url){
+  // code...
+$url_finde_page = pars_find($url);
+pars_car($url_finde_page);
+
+//var_dump($url_finde_page);
+
+}
+
+/* принимает массив от парсинга поиска и заходит в объявление*/
+function pars_car($array_link){
+  //var_dump($array_link);
+  $i = 0;
+  foreach ($array_link[1] as $link){
+      // code...
+      $i++;
+      $work_link = 'https://m.avito.ru'.$link;
+      $ad = file_get_contents($work_link);
+    //  print_r($ad);
+            //                                                          ограничение на Количество итераций!!!!!!!!!!!!!!!!!!!
+      if($i > 1) break;
+      //$w = derban_json($ad);
+      //print_r($w);
+      $obj_auto = find_data_avto($ad);
+      var_dump(derban_json($obj_auto));
+
+  }
+  //var_dump($ad);
+}
 /* принимает урл поиска и дергает линки объявлений*/
 function pars_find($url_name){
   $test=file_get_contents($url_name);
   $match = '/itemprop="url"
  href=\"(.{3,})\"/';
   preg_match_all($match, $test, $text);
+  //var_dump($text);
    return $text;
 
-}
-/* принимает массив от парсинга поиска и заходит в объявление*/
-function pars_car($array_link){
-  //var_dump($array_link);
-  foreach ($array_link[1] as $link){
-      // code...
-      $work_link = 'https://m.avito.ru'.$link;
-      $ad = file_get_contents($work_link);
-      print_r($ad);
-
-  }
 }
   /*получаем страницу для парсинга возвращаем данные в jsovn для работы */
 function find_data_avto($data){
@@ -29,5 +51,5 @@ function find_data_avto($data){
 /*разбираем json для сохранения или сразу сохраняем пока не знаю*/
 function derban_json($data_in_avto){
   $obj_avto = json_decode($data_in_avto);
-return $obj_avto;
+  return $obj_avto;
 }
